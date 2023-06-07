3e59b2a24b329855bc54835c263e11e682dfd3ef
Yuta Katayama <yutapilafu@gmail.com>
2023-06-07 14:56:57 +0900

    feat: IS_DOCKERで分岐するようにする

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
 	pageLoadTimeout: 75000, // 70s
 	defaultCommandTimeout: 65000, // 65s
 	env: {
+		IS_DOCKER: process.env.IS_DOCKER,
 		GOOGLE_ACCOUNT_ID: process.env.GOOGLE_ACCOUNT_ID,
 		GOOGLE_ACCOUNT_PASSWORD: process.env.GOOGLE_ACCOUNT_PASSWORD
 	},
